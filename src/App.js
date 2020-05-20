a428df2a4ec31d4d59a4b0972d7e12cc6076c9a5
Adam Hall <adamhalldesigns@gmail.com>
2020-05-20 11:28:49 +0100

    simple start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      const results = await axios.get('https://rickandmortyapi.com/api/character')
+      console.log(results.data.results[0])
+      setCharacters(results.data.results)
+    }
+    fetchCharacters();
+  }, [])
+
+  console.log(characters)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {characters.map((character) => <Character
+        image={character.image}
+        name={character.name}
+        status={character.status}
+        species={character.species}
+        location={character.location.name}
+      />)}
+      <p>{characters.find((character) => character.name === "Rick Sanchez")?.status}</p>
+      
     </div>
   );
 }
 
 export default App;
+
+function Character({ image, name, status, species, location }) {
+  return (
+    <div>
+      <img src={image} alt=""/>
+      <h2>{name}</h2>
+      <h3>{status} {species}</h3>
+      <p>Last known location {location}</p>
+      <p>first seen in EPISODE NAME</p>
+    </div>
+  )
+}
